10f7d2ce478e1ab61298fc9f2c78becd59ba764c
sabine <sabine.embacher@brockmann-consult.de>
2013-04-03 14:39:22 +0200

    change dependency to BEAM 4.11

--- beam-decisiontree/src/main/java/org/esa/beam/decisiontree/DecisionTreeConfiguration.java
+++ beam-decisiontree/src/main/java/org/esa/beam/decisiontree/DecisionTreeConfiguration.java
@@ -8,10 +8,16 @@ import java.util.Map;
 
 import com.thoughtworks.xstream.io.xml.xppdom.Xpp3Dom;
 import com.thoughtworks.xstream.io.xml.xppdom.Xpp3DomBuilder;
-import com.bc.ceres.binding.dom.Xpp3DomElement;
+import com.bc.ceres.binding.dom.XppDomElement;
+import com.thoughtworks.xstream.io.xml.xppdom.XppDom;
+import com.thoughtworks.xstream.io.xml.xppdom.XppFactory;
+import org.xmlpull.mxp1.MXParserCachingStrings;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.wrapper.classic.StaticXmlPullParserWrapper;
+import org.xmlpull.v1.wrapper.classic.XmlPullParserDelegate;
 
 public class DecisionTreeConfiguration {
-	
+
 	private String name;
 	private Classification[] classes;
 	private DecisionVariable[] variables;
@@ -19,13 +25,15 @@ public class DecisionTreeConfiguration {
 	private Map<String, Classification> classMap;
 
 	public static DecisionTreeConfiguration fromXML(Reader inputReader) {
-		Xpp3Dom dom;
+		XppDom dom;
 		DecisionTreeConfiguration configuration = new DecisionTreeConfiguration();
 		DecisionTreeDomConverter converter = new DecisionTreeDomConverter();
 		try {
-			dom = Xpp3DomBuilder.build(inputReader);
-			final Xpp3DomElement xpp3DomElement = new Xpp3DomElement(dom);
-			converter.convertDomToValue(xpp3DomElement, configuration);
+            final XmlPullParser parser = XppFactory.createDefaultParser();
+            parser.setInput(inputReader);
+            dom = XppDom.build(parser);
+			final XppDomElement xppDomElement = new XppDomElement(dom);
+			converter.convertDomToValue(xppDomElement, configuration);
 		} catch (Exception e) {
 			return null;
 		}
@@ -39,7 +47,7 @@ public class DecisionTreeConfiguration {
 	public Classification getClass(String aName) {
 		return classMap.get(aName);
 	}
-	
+
 	public void setClasses(Classification[] classes) {
 		this.classes = classes;
 		classMap = new HashMap<String, Classification>(classes.length);
@@ -47,11 +55,11 @@ public class DecisionTreeConfiguration {
 			classMap.put(aClass.getName(), aClass);
 		}
 	}
-	
+
 	public void setVariables(DecisionVariable[] variables) {
 		this.variables = variables;
 	}
-	
+
 	public DecisionVariable[] getVariables() {
 		return variables;
 	}
@@ -71,13 +79,13 @@ public class DecisionTreeConfiguration {
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
 	public Decision[] getAllDecisions() {
 		List<Decision> decisions = new ArrayList<Decision>();
 		addDecisionToList(getRootDecisions(), decisions);
 		return decisions.toArray(new Decision[decisions.size()]);
 	}
-	
+
 	private void addDecisionToList(Decision decision, List<Decision> decisionList) {
 		decisionList.add(decision);
 		if (decision.getYesDecision() != null) {
